4f57cd9012990cb04f20a4d8ce146feb94841c65
Srishti Mishra <srishti.mishra@citrix.com>
2019-02-11 22:38:11 -0500

    Added project scaffolding, login and register routes and views

---
+++ Vue_Projects/RecipeApp/recipe-app/client/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Register from '../components/Register'
+import Login from '../components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+	routes: [
+		{
+			path: '/register',
+			name: 'register',
+			component: Register
+		},
+		{
+			path: '/',
+			name: 'login',
+			component: Login
+		}
+	]
+})
